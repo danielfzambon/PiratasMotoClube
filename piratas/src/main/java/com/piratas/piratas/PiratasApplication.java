809627e32da1d7bbbe67fbc5812d3e08d3cfbb3a
Daniel Zambon <danielfzambon@gmail.com>
2024-02-11 14:32:24 -0300

    iitial commit

---
+++ piratas/src/main/java/com/piratas/piratas/PiratasApplication.java
@@ -0,0 +1,16 @@
+package com.piratas.piratas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratasApplication.class, args);
+		
+		System.out.println("Olá mudo: ");
+		
+	}
+
+}
