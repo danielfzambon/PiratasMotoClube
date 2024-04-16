f64ca4158b56d1a601ba3eb390eb2865ed2bbb97
Daniel Zambon <danielfzambon@gmail.com>
2024-04-16 19:21:46 -0300

    removendo diretorio nao utilizado

--- piratas/java/com/devsuperior/aula/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.aula.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.aula.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
